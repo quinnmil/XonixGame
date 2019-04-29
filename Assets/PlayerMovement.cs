fb49b857a9efe9d278f91a08760b1d1123e06d35
Zac Sims <zsims@uoregon.edu>
2019-04-29 09:48:42 -0700

    updated movement to change based of OnTerritory variable

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float thrust;
 	private string lastPress;
+	public bool onTerritory = false;
 
 
 
@@ -13,33 +14,57 @@ public class PlayerMovement : MonoBehaviour
 
 	}
 	 
-	void Update(){ 
-	    if (Input.GetKey(KeyCode.RightArrow)
-	    	&& !Input.GetKey(KeyCode.UpArrow)  
-	    	&& !Input.GetKey(KeyCode.DownArrow)){
-	    	transform.Translate(0,0,thrust*Time.deltaTime);	 
-	    	lastPress = "right"; 	
-	    }
-	    if (Input.GetKey(KeyCode.LeftArrow)
-	    	&& !Input.GetKey(KeyCode.UpArrow)  
-	    	&& !Input.GetKey(KeyCode.DownArrow)){
-	    	transform.Translate(0,0,-thrust*Time.deltaTime);
-	    	lastPress = "left";
-	    }
+	void Update(){
 
-	    if (Input.GetKey(KeyCode.UpArrow)
-	    	&& !Input.GetKey(KeyCode.LeftArrow)  
-	    	&& !Input.GetKey(KeyCode.RightArrow)){
-	    	transform.Translate(-thrust*Time.deltaTime,0,0);
-	    	lastPress = "up";	    	
-	    }
 
-	    if (Input.GetKey(KeyCode.DownArrow)
-	    	&& !Input.GetKey(KeyCode.LeftArrow)  
-	    	&& !Input.GetKey(KeyCode.RightArrow)){
-	    	transform.Translate(thrust*Time.deltaTime,0,0);
-	    	lastPress = "down";
-	    }
+		if (onTerritory)
+			onTerritoryMove();
+		else
+			offTerritoryMove();
+	   
+ 	}
+
+
+ 	public void offTerritoryMove(){
+
+ 		// do not let player go in the opposite direction,
+ 		// and do not player move in more than one direction at once
+ 		if (lastPress != "left"){
+		    if (Input.GetKey(KeyCode.RightArrow)
+		    	&& !Input.GetKey(KeyCode.UpArrow)  
+		    	&& !Input.GetKey(KeyCode.DownArrow)){
+		    	transform.Translate(0,0,thrust*Time.deltaTime);	 
+		    	lastPress = "right"; 	
+		    }
+		}
+		if (lastPress != "right"){
+		    if (Input.GetKey(KeyCode.LeftArrow)
+		    	&& !Input.GetKey(KeyCode.UpArrow)  
+		    	&& !Input.GetKey(KeyCode.DownArrow)){
+		    	transform.Translate(0,0,-thrust*Time.deltaTime);
+		    	lastPress = "left";
+		    }
+		}
+
+	    if (lastPress != "down"){
+		    if (Input.GetKey(KeyCode.UpArrow)
+		    	&& !Input.GetKey(KeyCode.LeftArrow)  
+		    	&& !Input.GetKey(KeyCode.RightArrow)){
+		    	transform.Translate(-thrust*Time.deltaTime,0,0);
+		    	lastPress = "up";	    	
+		    }
+		}
+
+		if (lastPress != "up"){
+		    if (Input.GetKey(KeyCode.DownArrow)
+		    	&& !Input.GetKey(KeyCode.LeftArrow)  
+		    	&& !Input.GetKey(KeyCode.RightArrow)){
+		    	transform.Translate(thrust*Time.deltaTime,0,0);
+		    	lastPress = "down";
+		    }
+		}
+	    //if a key press didn't happen in this frame,
+	    // go the same direction last pressed
 	    if (lastPress == "right"){
 	    	transform.Translate(0,0,thrust*Time.deltaTime);
 	    }
@@ -52,7 +77,37 @@ public class PlayerMovement : MonoBehaviour
 	    if (lastPress == "down"){
 	    	transform.Translate(thrust*Time.deltaTime,0,0);
 	    }
-	   
+
+ 	}
+
+
+ 	public void onTerritoryMove(){
+ 		
+	    if (Input.GetKey(KeyCode.RightArrow)
+	    	&& !Input.GetKey(KeyCode.UpArrow)  
+	    	&& !Input.GetKey(KeyCode.DownArrow)){
+	    	transform.Translate(0,0,thrust*Time.deltaTime);	  	
+		}
+	
+	    if (Input.GetKey(KeyCode.LeftArrow)
+	    	&& !Input.GetKey(KeyCode.UpArrow)  
+	    	&& !Input.GetKey(KeyCode.DownArrow)){
+	    	transform.Translate(0,0,-thrust*Time.deltaTime);
+	    }
+
+	    if (Input.GetKey(KeyCode.UpArrow)
+	    	&& !Input.GetKey(KeyCode.LeftArrow)  
+	    	&& !Input.GetKey(KeyCode.RightArrow)){
+	    	transform.Translate(-thrust*Time.deltaTime,0,0);	    	
+	    }
+
+	    if (Input.GetKey(KeyCode.DownArrow)
+	    	&& !Input.GetKey(KeyCode.LeftArrow)  
+	    	&& !Input.GetKey(KeyCode.RightArrow)){
+	    	transform.Translate(thrust*Time.deltaTime,0,0);
+
+	    }
+
  	}
 
 }
