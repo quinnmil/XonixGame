b1c04629ed4b45b0cf8e16029c95ecd7acaa8784
Refael Yehuda <refaely@uoregon.edu>
2019-05-02 16:55:08 -0700

    enemy added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody rb;
+
+    public bool moveLeft;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (moveLeft)
+        {
+            Vector3 temp = transform.position;
+            temp.x -= moveSpeed * Time.deltaTime;
+            transform.position = temp;
+        }
+        else
+        {
+            Vector3 temp = transform.position;
+            temp.x += moveSpeed * Time.deltaTime;
+            transform.position = temp;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //rb.velocity = RandomVector(0f, 5f);
+        if (other.tag == "Territory" || other.tag == "LeftWall" 
+            || other.tag == "RightWall" || other.tag == "UpWall" || other.tag == "DownWall")
+        {
+            moveLeft = !moveLeft;
+        }
+
+    }
+}
