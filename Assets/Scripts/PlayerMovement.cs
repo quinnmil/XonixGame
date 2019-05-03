01992d5d03e2c205f4701c7540cf4cef9da87cc7
zacharyssims <zsims@uoregon.edu>
2019-05-02 21:40:27 -0700

    added boundary restrictions to OffTerritorymove

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -110,64 +110,94 @@ public class PlayerMovement : MonoBehaviour
 
  	public void offTerritoryMove()
     {
- 		// do not let player go in the opposite direction,
- 		// and do not player move in more than one direction at once
- 		if (lastPress != "left")
+        // do not let player go in the opposite direction,
+        // and do not player move in more than one direction at once
+        if (!rightWall)
         {
-		    if (Input.GetKey(KeyCode.RightArrow)
-		    	&& !Input.GetKey(KeyCode.UpArrow)  
-		    	&& !Input.GetKey(KeyCode.DownArrow)){
-		    	transform.Translate(0,0,thrust * Time.deltaTime);	 
-		    	lastPress = "right"; 	
-		    }
-		}
+            if (lastPress != "left")
+            {
+                if (Input.GetKey(KeyCode.RightArrow)
+                    && !Input.GetKey(KeyCode.UpArrow)
+                    && !Input.GetKey(KeyCode.DownArrow))
+                {
+                    transform.Translate(0, 0, thrust * Time.deltaTime);
+                    lastPress = "right";
+                }
+            }
+        }
 
-		if (lastPress != "right")
+        if (!leftWall)
         {
-		    if (Input.GetKey(KeyCode.LeftArrow)
-		    	&& !Input.GetKey(KeyCode.UpArrow)  
-		    	&& !Input.GetKey(KeyCode.DownArrow)){
-		    	transform.Translate(0,0,-thrust * Time.deltaTime);
-		    	lastPress = "left";
-		    }
-		}
+            if (lastPress != "right")
+            {
+                if (Input.GetKey(KeyCode.LeftArrow)
+                    && !Input.GetKey(KeyCode.UpArrow)
+                    && !Input.GetKey(KeyCode.DownArrow))
+                {
+                    transform.Translate(0, 0, -thrust * Time.deltaTime);
+                    lastPress = "left";
+                }
+            }
+        }
 
-	    if (lastPress != "down")
-        {
-		    if (Input.GetKey(KeyCode.UpArrow)
-		    	&& !Input.GetKey(KeyCode.LeftArrow)  
-		    	&& !Input.GetKey(KeyCode.RightArrow)){
-		    	transform.Translate(-thrust * Time.deltaTime, 0,0);
-		    	lastPress = "up";	    	
-		    }
-		}
 
-		if (lastPress != "up")
+        if (!upWall)
         {
-		    if (Input.GetKey(KeyCode.DownArrow)
-		    	&& !Input.GetKey(KeyCode.LeftArrow)  
-		    	&& !Input.GetKey(KeyCode.RightArrow)){
-		    	transform.Translate(thrust * Time.deltaTime, 0,0);
-		    	lastPress = "down";
-		    }
-		}
+            if (lastPress != "down")
+            {
+                if (Input.GetKey(KeyCode.UpArrow)
+                    && !Input.GetKey(KeyCode.LeftArrow)
+                    && !Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Translate(-thrust * Time.deltaTime, 0, 0);
+                    lastPress = "up";
+                }
+            }
+        }
+
+        if (!downWall)
+        {
+            if (lastPress != "up")
+            {
+                if (Input.GetKey(KeyCode.DownArrow)
+                    && !Input.GetKey(KeyCode.LeftArrow)
+                    && !Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Translate(thrust * Time.deltaTime, 0, 0);
+                    lastPress = "down";
+                }
+            }
+        }
         //if a key press didn't happen in this frame,
         // go the same direction last pressed
         if (lastPress == "right")
         {
-            transform.Translate(0, 0, thrust*Time.deltaTime);
+            if (!rightWall)
+            {
+                transform.Translate(0, 0, thrust * Time.deltaTime);
+            }
         }
         if (lastPress == "left")
         {
-            transform.Translate(0, 0, -thrust * Time.deltaTime);
+            if (!leftWall)
+            {
+                transform.Translate(0, 0, -thrust * Time.deltaTime);
+            }
         }
         if (lastPress == "up")
         {
-            transform.Translate(-thrust * Time.deltaTime, 0, 0);
+            if (!upWall)
+            {
+                transform.Translate(-thrust * Time.deltaTime, 0, 0);
+            }
         }
         if (lastPress == "down")
         {
-            transform.Translate(thrust * Time.deltaTime, 0, 0);
+            if (!downWall)
+            {
+                transform.Translate(thrust * Time.deltaTime, 0, 0);
+            }
+            
         }
 
 
