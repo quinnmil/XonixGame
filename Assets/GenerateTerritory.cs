4cb276e825639fcfea8eec665699250e879affd7
zacharyssims <zsims@uoregon.edu>
2019-05-02 21:43:49 -0700

    glass walls

---
+++ Assets/GenerateTerritory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerritory : MonoBehaviour
+{
+    //Vector3[] pathLocation = [];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("Path Location:" + transform.position);
+        
+    }
+}
---
+++ Assets/PlaneScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        print(mesh.bounds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
